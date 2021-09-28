7caa1753bc5c8401a92d6b82cbe32eed366f6d58
lb3361 <lb3361@nyu.edu>
2021-09-27 20:32:17 -0400

    imported fatfs R014a

---
+++ fatfs/source/MAIN.c
@@ -0,0 +1,20 @@
+#include "ff.h"
+
+int main() {
+	f_closedir((void*)0);
+	return 0;
+}
+
+void USB_disk_write() { }
+void MMC_disk_write() { }
+void RAM_disk_write() { }
+void USB_disk_read() { }
+void MMC_disk_read() { }
+void RAM_disk_read() { }
+void USB_disk_initialize() { }
+void MMC_disk_initialize() { }
+void RAM_disk_initialize() { }
+void USB_disk_status() { }
+void MMC_disk_status() { }
+void RAM_disk_status() { }
+
